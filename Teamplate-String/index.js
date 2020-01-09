ed4ac6b930b5db7a1761b9efce09af94d78c3ccf
nguyenhongthaj <nguyenhongthajii@gmail.com>
2020-01-09 10:53:31 +0700

    use--teamplate-string

---
+++ Teamplate-String/index.js
@@ -0,0 +1,14 @@
+// function greeting(name){
+//   return 'Say hi ' + name +'!!!';
+// }
+// greeting('Thai');
+
+// function greeting(name,age){
+//   return `SayHi xin chào ${name} age: ${age}`;
+// }
+// greeting('Thai',22);
+
+greeting = (name,age) => {
+ return console.log(`SayHi xin chào ${name} age: ${age}`);
+}
+greeting('Thai Nguyen',22);
